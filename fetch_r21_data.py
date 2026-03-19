f5e7d7caf8cbbbca66c65750f7fa1e056ba3d43b
indsleep2019 <54394924+indsleep2019@users.noreply.github.com>
2026-03-19 21:12:26 +0100

    Update fetch_r21_data.py

--- fetch_r21_data.py
+++ fetch_r21_data.py
@@ -97,7 +97,7 @@ cs = conn.cursor()
 
 cs.executemany(
     "INSERT INTO TRAIN_DELAY_DB.RAW.R21_GITHUB_STAGE "
-    '("DATE", "LINE", "JOURNEY_ID", "DELAY_MINUTES", "LOAD_TIMESTAMP") '
+    '("DATE", "LINE", "JOURNEY_ID", "STOP_ID", "DELAY_MINUTES", "LOAD_TIMESTAMP") '
     "VALUES (%s,%s,%s,%s,%s)",
     records
 )
